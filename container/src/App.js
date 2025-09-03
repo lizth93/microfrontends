2b9ce061ae69d6e1f8dfcda90d3079cccb7743b6
lizth93 <lizth93@hotmail.com>
2025-09-02 19:07:46 -0500

    Wrap App component in StylesProvider for improved styling management

--- container/src/App.js
+++ container/src/App.js
@@ -3,17 +3,24 @@ import MarketingApp from "../components/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 export default function App() {
+    const generateClassName = createGenerateClassName({
+        productionPrefix: "cont",
+    });
 
     return (
-        <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-                <Footer />
-            </div>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                    <Footer />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     );
 }
+
 
