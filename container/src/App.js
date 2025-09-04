825d1d6f0c70d34092fe9a91bd327da37d5d7cdc
lizth93 <lizth93@hotmail.com>
2025-09-04 17:09:19 -0500

    fix: implement lazy loading for MarketingApp and AuthApp components

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import MarketingApp from "../components/MarketingApp";
+import React, { lazy, suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import AuthApp from "../components/AuthApp";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const MarketingApp = lazy(() => import("../components/MarketingApp"));
+const AuthApp = lazy(() => import("../components/AuthApp"));
 
 export default function App() {
     const generateClassName = createGenerateClassName({
@@ -16,10 +17,12 @@ export default function App() {
             <BrowserRouter>
                 <div>
                     <Header />
-                    <Switch>
-                        <Route path="/auth" component={AuthApp} />
-                        <Route path="/" component={MarketingApp} />
-                    </Switch>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Switch>
+                            <Route path="/auth" component={AuthApp} />
+                            <Route path="/" component={MarketingApp} />
+                        </Switch>
+                    </Suspense>
                     <Footer />
                 </div>
             </BrowserRouter>
